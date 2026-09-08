427c33508348e5a20ae015ff094cd1c91425db09
weedagreyt <weelanggid@gmail.com>
2026-09-08 14:51:30 -0400

    Add case study contact CTA

--- app/work/dope-marketing/page.tsx
+++ app/work/dope-marketing/page.tsx
@@ -3,6 +3,7 @@ import SiteNav from "../../components/SiteNav";
 import SiteFooter from "../../components/SiteFooter";
 import Reveal from "../../components/Reveal";
 import CaseStudyStory from "../../components/CaseStudyStory";
+import CaseStudyCta from "../../components/CaseStudyCta";
 import styles from "../case-study.module.css";
 import local from "./dope.module.css";
 
@@ -61,6 +62,7 @@ export default function DopeMarketingPage() {
         </div>
       </article>
 
+      <Reveal><CaseStudyCta /></Reveal>
       <Reveal className={styles.next}><small>Next Project</small><a href="/work/yensanities"><h2>YENSANITIES</h2><img src={assets.next} alt="" /></a></Reveal>
       <SiteFooter />
     </main>
