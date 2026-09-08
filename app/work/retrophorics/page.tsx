b392f4b09068911320f289a93a28d581e2b8e361
weedagreyt <weelanggid@gmail.com>
2026-09-08 14:51:29 -0400

    Add case study contact CTA

--- app/work/retrophorics/page.tsx
+++ app/work/retrophorics/page.tsx
@@ -3,6 +3,7 @@ import SiteNav from "../../components/SiteNav";
 import SiteFooter from "../../components/SiteFooter";
 import Reveal from "../../components/Reveal";
 import CaseStudyStory from "../../components/CaseStudyStory";
+import CaseStudyCta from "../../components/CaseStudyCta";
 import CaseVideo from "./CaseVideo";
 import styles from "../case-study.module.css";
 import local from "./retrophorics.module.css";
@@ -68,6 +69,7 @@ export default function RetrophoricsPage() {
         </div>
       </article>
 
+      <Reveal><CaseStudyCta /></Reveal>
       <Reveal className={styles.next}><small>Next Project</small><a href="/work/art-exploration"><h2>ART &amp; EXPLORATION</h2><img src={assets.next} alt="" /></a></Reveal>
       <SiteFooter />
     </main>
